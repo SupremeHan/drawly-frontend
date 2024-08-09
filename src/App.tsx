6d25dee4e3b3510a1734504aa2dec05b55ccb499
Nikola Zivojinovic <zivojinovicn13@gmail.com>
2024-08-09 18:58:40 +0200

    testing

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,7 @@ function App() {
 		getData();
 	}, []);
 
-	return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+	return <h1 className="text-3xl font-bold underline">Great success!!</h1>;
 }
 
 export default App;
