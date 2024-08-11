774801aa401cf5a3508e7250b3f2449ab11180bb
Nikola Zivojinovic <zivojinovicn13@gmail.com>
2024-08-11 20:31:34 +0200

    Added routing, google oauth implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,32 @@
-import { useEffect } from 'react';
+// import { useEffect } from 'react';
+// import { CredentialResponse, GoogleLogin } from '@react-oauth/google';
 import './App.css';
 
 function App() {
-	async function getData() {
-		const url = 'http://localhost:3000/';
-		try {
-			const response = await fetch(url);
-			if (!response.ok) {
-				throw new Error(`Response status: ${response.status}`);
-			}
+	// async function getData() {
+	// 	const url = 'http://localhost:3000/';
+	// 	try {
+	// 		const response = await fetch(url);
+	// 		if (!response.ok) {
+	// 			throw new Error(`Response status: ${response.status}`);
+	// 		}
 
-			const json = await response.text();
-			console.log(json);
-		} catch (error) {
-			console.error(error);
-		}
-	}
+	// 		const json = await response.text();
+	// 		console.log(json);
+	// 	} catch (error) {
+	// 		console.error(error);
+	// 	}
+	// }
 
-	useEffect(() => {
-		getData();
-	}, []);
+	// useEffect(() => {
+	// 	getData();
+	// }, []);
 
-	return <h1 className="text-3xl font-bold underline">Great success!!</h1>;
+	return (
+		<div>
+			<h1 className="text-3xl font-bold underline">Great success!!</h1>;
+		</div>
+	);
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,31 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import LandingPage from './pages/LandingPage.tsx';
+import LoginPage from './pages/LoginPage.tsx';
+
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <LandingPage /> // !!TODO: add a landing page
+	},
+	{
+		path: '/login',
+		element: <LoginPage />
+	},
+	{
+		path: '/drafts',
+		element: <App />
+	}
+]);
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+	<GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+		<StrictMode>
+			<RouterProvider router={router} />
+		</StrictMode>
+	</GoogleOAuthProvider>
+);
